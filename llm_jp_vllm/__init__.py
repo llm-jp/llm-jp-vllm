c8aec00273df60d9469225981b88e2b78ad1b66d
Yusuke Oda <yusuke.oda@predicate.jp>
2026-06-11 17:33:23 +0900

    Fix vllm parser (#1)
    * fix reasoning parser
    
    * fix paths
    
    * fix ci

--- llm_jp_vllm/__init__.py
+++ llm_jp_vllm/__init__.py
@@ -1,7 +1,1 @@
 """Package definition of llm-jp-vllm."""
-
-from llm_jp_vllm.llmjp4_reasoning_parser import Llmjp4ReasoningParser
-
-__all__ = [
-    "Llmjp4ReasoningParser",
-]
---
+++ llm_jp_vllm/llmjp4/__init__.py
@@ -0,0 +1,7 @@
+"""Package root for llm_jp_vllm/llm_jp_4."""
+
+from llm_jp_vllm.llmjp4.reasoning_parser import Llmjp4ReasoningParser
+
+__all__ = [
+    "Llmjp4ReasoningParser",
+]
